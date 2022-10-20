3cd874a338ed29f84b18d04f35865b53a9f20476
Zhian N. Kamvar <zkamvar@gmail.com>
2022-10-20 10:58:40 -0700

    add some starting point and update renv

--- update-workflows.R
+++ update-workflows.R
@@ -2,3 +2,38 @@ library("gh")
 library("fs")
 library("gert")
 library("yaml")
+library("jsonlite")
+library("withr")
+library("purrr")
+
+
+source("functions.R")
+workbench_repos <- c("workbench-template-md", 
+  "workbench-template-rmd",
+  "sandpaper-docs", 
+  "lesson-development-training", 
+  "R-ecology-lesson", 
+  "CarpentriesOffline_Instructor_Onboarding",
+  "intro-data-viz",
+  "r-tidyverse-4-datasets",
+  "iot-novice",
+  "python-modeling-power-consumption",
+  "python-classifying-power-consumption",
+  "R-ecology-lesson-intermediate",
+  "encode-data-exploration"
+)
+
+# all official repos that were built with styles and are still active
+official_repos <- jsonlite::read_json("data/lessons.json") |>
+  purrr::discard(\(x) x$life_cycle == "on-hold" | x$repo %in% workbench_repos) |>
+  purrr::map(\(x) x[names(x) != "github_topics"]) 
+
+# all community repos that were built with styles or the template
+community_repos <- jsonlite::read_json("data/community_lessons.json") |>
+  purrr::discard(\(x) x$life_cycle == "on-hold" | x$repo %in% workbench_repos) |>
+  purrr::map(\(x) x[names(x) != "github_topics"])
+
+tmpdir <- setup_tmpdir()
+
+
+
